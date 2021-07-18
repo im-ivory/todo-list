01936631b91aa80d9849e38fec5f3c88c5a76545
im-ivory <78600276+im-ivory@users.noreply.github.com>
2021-07-19 03:36:29 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -3,24 +3,29 @@ const loginInput = document.querySelector("#loginForm input");
 
 const greeting = document.querySelector("#greeting");
 const HIDE_CLASSLIST = "hidden";
+const USERNAME_KEY = "username";
 
 function handleLogin(event){
     event.preventDefault();
     loginForm.classList.add(HIDE_CLASSLIST);
     const username = loginInput.value;
-    localStorage.setItem("username", username);  
-    greeting.innerText = `Hello ${username}`;  
-    greeting.classList.remove(HIDE_CLASSLIST);
+    localStorage.setItem(USERNAME_KEY, username);  
+    paintGreeting(username);
 };
 
 loginForm.addEventListener("submit", handleLogin);
 
-const savedUsername = localStorage.getItem("username");
+function paintGreeting(name){
+    greeting.innerText = `Hello ${name}`;  
+    greeting.classList.remove(HIDE_CLASSLIST);
+}
+
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if(savedUsername === null){
     loginForm.classList.remove(HIDE_CLASSLIST);
     loginForm.addEventListener("submit", handleLogin);
 } else {
-    greeting.innerText = `Hello ${savedUsername}`; 
-    greeting.classList.remove(HIDE_CLASSLIST);
-}
+    paintGreeting(savedUsername);
+};
